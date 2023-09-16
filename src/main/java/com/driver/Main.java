619a0d26fc399b9d5f4055b3d187a552ae10b324
adnan <adnan232462@gmail.com>
2023-09-16 11:34:14 +0530

    correction

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,12 +10,13 @@ public class Main {
     public static class B extends A{
         @Override
         public String meth(){
-            return "Method this overridden method from obj of class B";
+            return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
+
         B obj1 = new B();
         obj1.meth();
     }
