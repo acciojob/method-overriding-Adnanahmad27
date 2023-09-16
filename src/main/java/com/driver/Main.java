d16515b9879a57e90c57b1c25d73ed683f4d1f5c
adnan <adnan232462@gmail.com>
2023-09-16 11:27:01 +0530

    overriding meth

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method this overridden method from obj of class B";
+        }
+    }
     public static void main(String[] args) {
-        ClassB obj = new ClassB();
-        System.out.println(obj.meth());
+        B obj = new B();
+        obj.meth();
+        B obj1 = new B();
+        obj1.meth();
     }
   
 }
-class ClassA{
-    public String meth(){
-        return "Invoking method from class A";
-    }
 
-}
-class ClassB extends ClassA{
-    @Override
-    public String meth(){
-        return "Method this overridden method from obj of class B";
-    }
-}
